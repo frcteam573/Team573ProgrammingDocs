755087ce0e23e8e0e690cb7400888cedc1fbffb8
Eric Savage <savage301@gmail.com>
2025-12-12 14:51:08 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Team 573 Read the Docs'
+copyright = '2025, FRC Team 573'
+author = 'FRC Team 573'
 
 release = '0.1'
 version = '0.1.0'
